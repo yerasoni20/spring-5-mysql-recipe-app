17a47626b666f79c628cd4181b4a12e0e72226ca
yerasoni20 <32177481+yerasoni20@users.noreply.github.com>
2022-05-03 04:53:54 -0400

    Mysql Configurations added

--- src/main/java/guru/springframework/services/UnitOfMeasureService.java
+++ src/main/java/guru/springframework/services/UnitOfMeasureService.java
@@ -4,9 +4,7 @@ import guru.springframework.commands.UnitOfMeasureCommand;
 
 import java.util.Set;
 
-/**
- * Created by jt on 6/28/17.
- */
+
 public interface UnitOfMeasureService {
 
     Set<UnitOfMeasureCommand> listAllUoms();
